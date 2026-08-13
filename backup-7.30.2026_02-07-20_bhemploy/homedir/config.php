5c56a6914a037e0957c39c041e12ea4d70d8e832
Himendra Bohara <hemendra.bohara@gmail.com>
2026-08-12 20:09:48 -0400

    Add main configuration file for application

---
+++ backup-7.30.2026_02-07-20_bhemploy/homedir/config.php
@@ -0,0 +1,11 @@
+<?php
+// Main Configuration File
+session_start();
+
+// Database credentials (if applicable)
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+?>
